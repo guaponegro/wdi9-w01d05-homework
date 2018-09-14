47838ee0f16e55acd8890ad85131ccf3048d4417
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-09-14 15:27:41 -0600

    IA part of the hw completed

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// 1. Variables & Datatypes
+
+// A. Q + A
+// i. We assign a value to a variable by using 1 = sign
+// ex. let x = 5;
+
+// ii. We change the value of that variable by setting the
+// variable equal to something else. Ex. let x = 5; to reassign it
+// you could say x = "car";
+
+// iii. Say we have 2 seperate variables, let x = 5; and let y = 10;
+// If we change the value of x from 5 to 10, we can then write let x = y
+// because they both have the same value
+
+// iv. Declaring something registers a variable in its scope.
+// Assigning something giving a variable a specific value 
+// Defining something gives a variable its parameters
+
+// v. Pseudocode is writing out what you want to do in plain text
+// before you actually write the code. You should do it because
+// it gives you direction so that you do not write a bunch of useless code
+
+// vi. You should be spending 75-80% of your time thinking about how to solve
+// the problem and 25-20% of your time typing the actual code
+
