c519063d8146cb652e73a3329a19be13f0735a2e
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-09-14 16:28:40 -0600

    IIC part of hw completed

--- js/app.js
+++ js/app.js
@@ -86,8 +86,17 @@
 // }
 
 // B. Get even
-for(let i = 1; i < 100; i++){
-    if(i % 2 === 0){
-        console.log(i + ` <-- is an even number`);
+// for(let i = 1; i < 100; i++){
+//     if(i % 2 === 0){
+//         console.log(i + ` <-- is an even number`);
+//     }
+// }
+
+// C. Give me Five
+for(let i = 1; i <= 100; i++){
+    if(i % 5 === 0){
+        console.log(`I found a ${i}. High five!`);
+    } if(i % 3 === 0){
+        console.log(`I found a ${i}. Three is a crowd`);
     }
 }
