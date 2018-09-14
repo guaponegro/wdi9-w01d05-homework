b05dbe89cf5a1716cf2bfd4bdb667ccd94cf8bc9
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-09-14 17:55:11 -0600

    IIIH part of hw completed

--- js/app.js
+++ js/app.js
@@ -162,11 +162,54 @@
 // }
 
 // G. Monkey in Middle
-const num = 3;
-if(num < 5){
-    console.log("little number");
-} else if(num > 10){
-    console.log("big number");
-} else{
-    console.log("monkey");
-}
+// const num = 3;
+// if(num < 5){
+//     console.log("little number");
+// } else if(num > 10){
+//     console.log("big number");
+// } else{
+//     console.log("monkey");
+// }
+
+// H. What's in Your Closet?
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "GA hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+  
+  // Thom's closet is more complicated. Check out this nested data structure!!
+  const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
+
+  console.log(`Kristyn is rocking that ${kristynsCloset[2]} today!`);
+  kristynsCloset.splice(6, 0, "raybans");
+  kristynsCloset[5] = "stained knit hat";
+  
+  console.log(thomsCloset[0][0]);
+  console.log(thomsCloset[1][1]);
+  console.log(thomsCloset[2][0]);
+  console.log(`Thom is looking fierce in a ${thomsCloset[0][0]}, ${thomsCloset[1][1]}, and ${thomsCloset[2][1]}!`);
+  thomsCloset[1][2] = "Footie Pajamas";
+  console.log(thomsCloset[1]);
